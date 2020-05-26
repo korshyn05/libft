e2071a7183184e26f0e9589a6e9ae36f47ef2163
ikorsun <i.korsun@ticketland.ru>
2020-05-27 00:40:01 +0300

    fix

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,25 +6,34 @@
 /*   By: tludwig <tludwig@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 19:31:47 by tludwig           #+#    #+#             */
-/*   Updated: 2020/05/27 00:06:10 by tludwig          ###   ########.fr       */
+/*   Updated: 2020/05/27 00:39:41 by tludwig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_atoi(const char *nptr)
+static int	check(int n, int size, int sign)
+{
+	if (size > 10 && sign == 1)
+		return (-1);
+	if (size > 10 && sign == -1)
+		return (0);
+	return (n * sign);
+}
+
+int			ft_atoi(const char *nptr)
 {
 	int n;
 	int sign;
+	int size;
 
 	n = 0;
 	sign = 1;
+	size = 0;
 	while (*nptr == ' ' || *nptr == '\t' || *nptr == '\n' || *nptr == '\v'
 			|| *nptr == '\r' || *nptr == '\f')
 		nptr++;
-	if ((*nptr < '0' || *nptr > '9') && *nptr != '-' && *nptr != '+')
-		return (0);
-	else if (*nptr == '-')
+	if (*nptr == '-')
 	{
 		sign = -1;
 		nptr++;
@@ -34,9 +43,8 @@ int		ft_atoi(const char *nptr)
 	while (*nptr >= '0' && *nptr <= '9')
 	{
 		n = 10 * n + (*nptr - '0');
-		if (n < 0 && n != -2147483648)
-			return (sign == 1 ? 0 : -1);
 		nptr++;
+		size++;
 	}
-	return (n * sign);
+	return (check(n, size, sign));
 }
